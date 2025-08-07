ee008ae47548b9ff52e68c3096beebe62637f604
2225244153 <2225244153@qq.com>
2025-08-07 18:52:48 +0800

    基本设置UI

--- Source/FirePlanet/Private/UMG/Setting/OptionUserWidget.cpp
+++ Source/FirePlanet/Private/UMG/Setting/OptionUserWidget.cpp
@@ -3,3 +3,36 @@
 
 #include "UMG/Setting/OptionUserWidget.h"
 
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "Framework/MainMenuLevel/MainMenuHUD.h"
+
+void UOptionUserWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	Button_NormalOption->OnClicked.AddDynamic(this,&UOptionUserWidget::NormalOptionClicked);
+	Button_InputOption->OnClicked.AddDynamic(this,&UOptionUserWidget::InputOptionClicked);
+	Button_Esc->OnClicked.AddDynamic(this,&UOptionUserWidget::EscClicked);
+}
+
+void UOptionUserWidget::NormalOptionClicked()
+{
+	Button_InputOption->SetRenderOpacity(1.0f);
+	Button_NormalOption->SetRenderOpacity(0.5f);
+	Switcher_Option->SetActiveWidgetIndex(0);
+}
+
+void UOptionUserWidget::InputOptionClicked()
+{
+	Button_NormalOption->SetRenderOpacity(1.0f);
+	Button_InputOption->SetRenderOpacity(0.5f);
+	Switcher_Option->SetActiveWidgetIndex(1);
+}
+
+void UOptionUserWidget::EscClicked()
+{
+	if (AMainMenuHUD* MainMenuHUD =  Cast<AMainMenuHUD>(GetOwningPlayer()->GetHUD()))
+	{
+		MainMenuHUD->OptionUserWidget = nullptr;
+	}
+}
--- Source/FirePlanet/Public/UMG/Setting/OptionUserWidget.h
+++ Source/FirePlanet/Public/UMG/Setting/OptionUserWidget.h
@@ -6,6 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "OptionUserWidget.generated.h"
 
+class UWidgetSwitcher;
+class UButton;
 /**
  * 
  */
@@ -13,5 +15,29 @@ UCLASS()
 class FIREPLANET_API UOptionUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+
+	virtual void NativeOnInitialized() override;
+
+	UFUNCTION()
+	void NormalOptionClicked();
+	UFUNCTION()
+	void InputOptionClicked();
+	UFUNCTION()
+	void EscClicked();
 	
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	UButton* Button_NormalOption;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* Button_InputOption;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* Button_Esc;
+
+	UPROPERTY(meta = (BindWidget))
+	UWidgetSwitcher* Switcher_Option;
 };
